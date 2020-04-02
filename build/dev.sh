f71ba0ff18b62def58f468c9f05ba0c86305adbc
whitevoid <381848900@qq.com>
2020-04-02 21:37:57 +0800

    feat: 编译运行shell最终版

--- build/dev.sh
+++ build/dev.sh
@@ -11,13 +11,13 @@ echo "编译完成"
 ps -aux|grep yhy-run| grep -v grep | awk '{print $2}' | xargs -r kill -9
 echo "关闭进程"
 # 重新启动
-screen -dm "$(./output/yhy-run)"
-let try_num=0
+nohup ./output/yhy-run >/var/log/yhy.log 2>&1 &
+try_num=0
 while([ ! -n "$(ps -aux|grep yhy-run| grep -v grep | awk '{print $2}')" ])
 do
     sleep 1
-    try_num+=1
-    if [ "$try_num" -lt 60 ]
+    let try_num+=1
+    if [ "$try_num" -lt 30 ]
     then
         echo "重启进程中...($try_num)"
     else
@@ -26,6 +26,5 @@ do
     fi
 done
 sleep 3
-screen -dR
 echo "启动成功"
 exit 0
