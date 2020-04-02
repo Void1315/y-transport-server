5169bc2df555c420b2e1fb3802977572a22308d4
whitevoid <381848900@qq.com>
2020-04-02 16:58:00 +0800

    feat: 加入文字提示

--- build/dev.sh
+++ build/dev.sh
@@ -6,9 +6,10 @@ git pull
 # 编译go文件
 go build -o ./output/yhy-run
 chmod 777 ./output/yhy-run
-
+echo "编译完成"
 # 关闭原先的go进程
 ps -aux|grep yhy-run| grep -v grep | awk '{print $2}' | xargs -r kill -9
-
+echo "关闭进程"
 # 重新启动
 nohup ./output/run &
+echo "重启进程"
